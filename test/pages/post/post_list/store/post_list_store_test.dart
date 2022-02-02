7f9f2da62ad73bdb1406966d3aa95c477101aed7
bruno.noveli <brunimzuppwolf@gmail.com>
2022-02-02 17:12:42 -0300

    Anotação extra e descrição dos métodos a serem testados de post_list_store

---
+++ test/pages/post/post_list/store/post_list_store_test.dart
@@ -0,0 +1,35 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:treinamento_get_it/pages/post/post_list/store/post_list_store.dart';
+
+void main() {
+
+  late PostListStore store;
+
+  setUp(() {
+    store = PostListStore();
+  });
+
+  test("Should set a list of Posts inside variable 'posts' when the method 'setPosts' is called", () {
+
+  });
+
+  test("Should clear the list of variable 'posts' when the method 'clearPosts' is called", () {
+
+  });
+
+  test("Should set variable 'isLoading' to true when 'setLoading(true)' is called", () {
+
+  });
+
+  test("Should set variable 'isLoading' to false when 'setLoading(false)' is called", () {
+
+  });
+
+  test("Should set a object from type RestException inside variable 'error' when 'setError' is called", () {
+
+  });
+
+  test("Should clear the variable error setting it to null", () {
+
+  });
+}
